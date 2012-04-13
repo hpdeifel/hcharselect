0b6322eace3cae5aaafea1fd7dc07a192d4cfbae
Hans-Peter Deifel <hpdeifel@gmx.de>
2012-04-13 12:29:12 +0200

    New search algorithm (untested)

--- HCharselect/Search.hs
+++ HCharselect/Search.hs
@@ -5,7 +5,14 @@ import Data.List
 
 import HCharselect.Character
 
-strMatcher str (Character n _ as) = any match (n:as)
-  where match b = remBag str `isInfixOf` remBag b
+strMatcher str (Character n _ as) = any (match str) (n:as)
+
+match needle haystack = prep needle `matchWords` prep haystack
+  where prep = words . remBag
         remBag = filter (not . flip elem ignore) . map toUpper
-        ignore = " ,.\t'-_"
+        ignore = ",.'-_"
+
+matchWords [] _ = True
+matchWords _ [] = False
+matchWords (n:ns) (h:hs)
+  = if n `isInfixOf` h then matchWords ns hs else matchWords (n:ns) hs
